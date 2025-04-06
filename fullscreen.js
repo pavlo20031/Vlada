e534f701c9a6cad0eb8aadb396f566d300b4f547
pavlo20031 <lesivpavlo999@gmail.com>
2025-04-06 18:00:32 +0300

    Update fullscreen.js

--- fullscreen.js
+++ fullscreen.js
@@ -1,1 +1,10 @@
-
+document.addEventListener("click", () => {
+  const docElm = document.documentElement;
+  if (docElm.requestFullscreen) {
+    docElm.requestFullscreen();
+  } else if (docElm.webkitRequestFullscreen) {
+    docElm.webkitRequestFullscreen();
+  } else if (docElm.msRequestFullscreen) {
+    docElm.msRequestFullscreen();
+  }
+}, { once: true });
